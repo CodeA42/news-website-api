4b61c62392115e932ca35a8c5f9fad29cd30f5be
Aleksandar Velichkov <sashko98@gmail.com>
2021-07-08 17:25:17 +0300

    initial server config

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
 const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+//Init db
+require("./db/db");
 
-const uri = process.env.DB_URI;
-mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-});
-const connection = mongoose.connection;
-connection.once("open", () => {
-  console.log("MongoDB databese connection established successfully");
-});
+//Init router
+const router = require("./routes/router");
 
-app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`);
-});
+//Init express
+require("./express")(app);
+app.use(router);
