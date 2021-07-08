01954ca7f14a5c963fba3bf3eb293f9910ce2d0e
Aleksandar Velichkov <sashko98@gmail.com>
2021-07-08 17:30:36 +0300

    -added router

--- routes/router.js
+++ routes/router.js
@@ -1,2 +1,14 @@
-const { Router } = require("express");
-const router = Router();
+const router = require("express").Router();
+
+router.use("/users", require("./user/controller"));
+
+//Article controllers
+router.use("/articles", require("./articles/get"));
+router.use("/articles", require("./articles/post"));
+router.use("/articles", require("./articles/put"));
+router.use("/articles", require("./articles/delete"));
+
+router.use("*", (req, res) => {
+  res.status(404).json("Invalid request");
+});
+module.exports = router;
