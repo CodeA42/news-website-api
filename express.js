63d1f6dbea3203ea09f99350171532fd9fe6ff1d
Aleksandar Velichkov <sashko98@gmail.com>
2021-07-08 17:24:57 +0300

    express initial setup

---
+++ express.js
@@ -0,0 +1,15 @@
+const cors = require("cors");
+const express = require("express");
+
+const port = process.env.PORT || 5000;
+
+function configExpress(app) {
+  app.use(cors());
+  app.use(express.json());
+
+  app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+  });
+}
+
+module.exports = configExpress;
