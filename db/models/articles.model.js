007cfe74037054d8007a9a4b893261d62c01a3ec
Aleksandar Velichkov <sashko98@gmail.com>
2021-07-08 17:28:16 +0300

    -added article model

---
+++ db/models/articles.model.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const articleSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    body: { type: String, required: true },
+    postDate: { type: Date, default: Date.now },
+    authors: [String],
+    postedBy: { type: String, required: true },
+    comments: [String],
+  },
+  { timestamps: true }
+);
+
+const Article = mongoose.model("Article", articleSchema);
+
+module.exports = Article;
