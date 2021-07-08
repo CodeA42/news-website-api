6c5734ad43fed910d3b4032a9cd5d415d3be5d72
Aleksandar Velichkov <sashko98@gmail.com>
2021-07-08 17:30:03 +0300

    -added "article/:id" path -added "article/" path

---
+++ routes/articles/get.js
@@ -0,0 +1,28 @@
+const router = require("express").Router();
+const Article = require("../../db/models/articles.model");
+
+router.get("/:id", async (req, res) => {
+    try {
+      const article = await Article.findById(req.params.id).exec();
+      let articleObj = article.toObject();
+  
+      res.status(200).json(articleObj);
+    } catch (e) {
+      res.status(404).json("Not found");
+    }
+  
+});
+
+router.get("/", (req, res) => {
+  try {
+    Article.find().exec(function(err, articles){
+      res.status(200).json(articles);
+    });
+    
+  } catch(e) {
+    console.error(e);
+    res.status(404).json("Err");
+  }
+});
+  
+module.exports = router;
