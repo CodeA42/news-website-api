8c8fda8d7ca25fdf503cf3a8f81a398af7101e4f
Aleksandar Velichkov <sashko98@gmail.com>
2021-09-22 17:11:44 +0300

    -added express session to express config

--- express.js
+++ express.js
@@ -1,5 +1,6 @@
 const cors = require("cors");
 const express = require("express");
+const session = require('express-session');
 
 const port = process.env.PORT || 5000;
 
@@ -7,6 +8,11 @@ function configExpress(app) {
   app.use(cors());
   app.use(express.json());
 
+  app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+  }));
   app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
   });
