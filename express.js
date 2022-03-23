72a8156ab6ea5b04461c8d5b38665ebe1b728277
Aleksandar Velichkov <sashko98@gmail.com>
2022-03-23 11:56:06 +0200

    -removed passport from express setup

--- express.js
+++ express.js
@@ -1,6 +1,5 @@
 const cors = require("cors");
 const express = require("express");
-const passport = require('passport');
 const session = require('express-session');
 
 const port = process.env.PORT || 5000;
@@ -9,13 +8,6 @@ function configExpress(app) {
   app.use(cors());
   app.use(express.json());
 
-  app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-  }));
-  app.use(passport.initialize());
-  app.use(passport.session())
   app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
   });
